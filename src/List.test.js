acef002cb917d46d6789f725fbf990bf6c8f8f02
Stacey Sugiono <sugiono.stacey@gmail.com>
2019-09-18 17:46:38 -0700

    fixed List test

--- src/List.test.js
+++ src/List.test.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import List from './List';
-import renderer from 'react-test-renderer';
 
 describe('List component:', () => {
     it('renders without crashing', () => {
         const div = document.createElement('div');
-        ReactDOM.render(<List />, div);
+        ReactDOM.render(<List cards={[]}/>, div);
         ReactDOM.unmountComponentAtNode(div);
     })
 
